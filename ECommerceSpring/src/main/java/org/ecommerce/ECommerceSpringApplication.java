2322a0d1e0160dba42eff40183b24cc265aed93f
Onur BAŞ <onurbass.ob@gmail.com>
2024-03-09 23:05:16 +0300

    Let's get start

---
+++ ECommerceSpring/src/main/java/org/ecommerce/ECommerceSpringApplication.java
@@ -0,0 +1,12 @@
+package org.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringApplication {
+
+  public static void main(String[] args) {
+	SpringApplication.run(ECommerceSpringApplication.class,args);
+  }
+}
